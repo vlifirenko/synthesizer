ba1858fea8a8820f3bd968a76ffb2a65f5c921b6
Vladislav <vladislav.lifirenko@gmail.com>
2024-07-08 15:09:38 +0300

    Sin and square

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 #include "olcNoiseMaker.h"
 
+atomic<double> dFrequencyOutput = 0.0;
+
+double MakeNoise(double dTime)
+{
+   double dOutput = 1.0 * sin(dFrequencyOutput * 2 * 3.14159 * dTime);
+
+   if (dOutput > 0.0)
+      return 0.2;
+   else
+      return -0.2;
+}
+
 int main()
 {
    wcout << "Synthesizer" << endl;
@@ -16,6 +28,32 @@ int main()
 
    // create noise machine
    olcNoiseMaker<short> sound(devices[0], 44100, 1, 8, 512);
+
+   // link noise function with sound machine
+   sound.SetUserFunction(MakeNoise);
+
+   double dOctaveBaseFrequency = 110.0;   // A2
+   double d12thRootOf2 = pow(2.0, 1.0 / 12.0);
+
+   while (1)
+   {
+      // keyboard
+
+      bool bKeyPressed = false;
+      for (int k = 0; k < 15; k++)
+      {
+         if (GetAsyncKeyState((unsigned char)("ZSXCFVGBNJMK\xbcL\xbe"[k])) & 0x8000)
+         {
+            dFrequencyOutput = dOctaveBaseFrequency * pow(d12thRootOf2, k);
+            bKeyPressed = true;
+         }
+      }
+
+      if (!bKeyPressed)
+      {
+         dFrequencyOutput = 0.0;
+      }
+   }
 
    return 0;
 }
